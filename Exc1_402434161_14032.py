49dded031c35976a2302aed5c3780128d7d3b350
Narges <s0ltanian3099@gmail.com>
2025-03-08 19:10:01 +0000

    Done.

---
+++ Exc1_402434161_14032.py
@@ -0,0 +1,10 @@
+name = input("Lotfan nam khod ra vared konid: ")
+age = input("Lotfan sen khod ra vared konid: ")
+height = input("Lotfan ghad khod ra be santi-metr vared konid: ")
+
+print("\nEttela'at shoma:")
+print(f"Nam: {name}")
+print(f"Sen: {age} sal")
+print(f"Ghad: {height} santi-metr")
+
+input("Baraye khoroj, yek kelid ra feshar dahid...")
